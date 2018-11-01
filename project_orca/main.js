26ab251abf3a47ba66250a4ec0fe33efa0ab943b
Milan Iliev <milaniliev@gmail.com>
2018-11-01 00:13:31 -0400

    Access Mode

--- project_orca/main.js
+++ project_orca/main.js
@@ -5,9 +5,7 @@ document.getElementById("login_button").addEventListener("click", () => {
 document.querySelector(".equation input").addEventListener("input", () => {
   if(document.querySelector(".equation input").value === 'P(B|A) x P(A)'){
     setTimeout(() => {
-      document.getElementById("login").style.display = ""
-      document.getElementById("login").querySelector("[name=username]").value = "gpackard"
-      document.getElementById("login").querySelector("[name=password]").value = "fasdagsad"
+      document.body.parentElement.classList.add('access')
     }, 500)
   }
 })
