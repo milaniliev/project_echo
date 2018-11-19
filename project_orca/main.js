1aba5612a62157ae83cd7d3a6d900b30825aa886
Milan Iliev <milaniliev@gmail.com>
2018-11-19 11:50:58 -0500

    Backup.txt

--- project_orca/main.js
+++ project_orca/main.js
@@ -4,8 +4,11 @@ document.getElementById("login_button").addEventListener("click", () => {
 
 document.querySelector(".equation input").addEventListener("input", () => {
   if(document.querySelector(".equation input").value === 'P(B|A) x P(A)'){
-    setTimeout(() => {
+    setTimeout(async () => {
       document.body.parentElement.classList.add('access')
+      let response = await fetch("backup.txt")
+      let archive_text = await response.text()
+      document.querySelector('.goals').innerText = archive_text
     }, 500)
   }
 })
